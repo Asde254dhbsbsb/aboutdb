2046aabdc0e03452cde7daa0cf2a825f92f10a50
yanzhang <1390115638@qq.com>
2022-07-25 12:47:28 +0800

    delete .git

---
+++ sp22-rookiedb/src/main/java/edu/berkeley/cs186/database/query/join/GHJOperator.java
@@ -0,0 +1,287 @@
+package edu.berkeley.cs186.database.query.join;
+
+import edu.berkeley.cs186.database.TransactionContext;
+import edu.berkeley.cs186.database.common.HashFunc;
+import edu.berkeley.cs186.database.common.Pair;
+import edu.berkeley.cs186.database.common.iterator.BacktrackingIterator;
+import edu.berkeley.cs186.database.databox.DataBox;
+import edu.berkeley.cs186.database.databox.IntDataBox;
+import edu.berkeley.cs186.database.query.JoinOperator;
+import edu.berkeley.cs186.database.query.QueryOperator;
+import edu.berkeley.cs186.database.query.disk.Partition;
+import edu.berkeley.cs186.database.query.disk.Run;
+import edu.berkeley.cs186.database.table.Record;
+import edu.berkeley.cs186.database.table.Schema;
+
+import java.util.*;
+
+public class GHJOperator extends JoinOperator {
+    private int numBuffers;
+    private Run joinedRecords;
+
+    public GHJOperator(QueryOperator leftSource,
+                       QueryOperator rightSource,
+                       String leftColumnName,
+                       String rightColumnName,
+                       TransactionContext transaction) {
+        super(leftSource, rightSource, leftColumnName, rightColumnName, transaction, JoinType.GHJ);
+        this.numBuffers = transaction.getWorkMemSize();
+        this.stats = this.estimateStats();
+        this.joinedRecords = null;
+    }
+
+    @Override
+    public int estimateIOCost() {
+        // Since this has a chance of failing on certain inputs we give it the
+        // maximum possible cost to encourage the optimizer to avoid it
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean materialized() { return true; }
+
+    @Override
+    public BacktrackingIterator<Record> backtrackingIterator() {
+        if (joinedRecords == null) {
+            // Executing GHJ on-the-fly is arduous without coroutines, so
+            // instead we'll accumulate all of our joined records in this run
+            // and return an iterator over it once the algorithm completes
+//            执行GHJ on-the-fly是很困难的在没有协同程序的情况下，所有代替的是在整个运行中累计所有连接记录
+            this.joinedRecords = new Run(getTransaction(), getSchema());
+            this.run(getLeftSource(), getRightSource(), 1);
+        };
+        return joinedRecords.iterator();
+    }
+
+    @Override
+    public Iterator<Record> iterator() {
+        return backtrackingIterator();
+    }
+
+    /**
+     * For every record in the given iterator, hashes the value
+     * at the column we're joining on and adds it to the correct partition in
+     * partitions.
+     *  对于给定迭代器中的每一条记录，将我们要连接的列的值散列，并将其添加到分区中的正确分区。
+     * @param partitions an array of partitions to split the records into
+     * @param records iterable of records we want to partition
+     * @param left true if records are from the left relation, otherwise false
+     * @param pass the current pass (used to pick a hash function)
+     */
+    private void partition(Partition[] partitions, Iterable<Record> records, boolean left, int pass) {
+        // TODO(proj3_part1): implement the partitioning logic
+        // You may find the implementation in SHJOperator.java to be a good
+        // starting point. You can use the static method HashFunc.hashDataBox
+        // to get a hash value.
+        for(Record record : records) {
+            DataBox columnValue = record.getValue(left ? getLeftColumnIndex() : getRightColumnIndex());
+            int hash = HashFunc.hashDataBox(columnValue, pass);
+            int partitionNum = hash % partitions.length;
+            if(partitionNum < 0) partitionNum += partitions.length;
+            partitions[partitionNum].add(record);
+        }
+    }
+
+    /**
+     * Runs the buildAndProbe stage on a given partition. Should add any
+     * matching records found during the probing stage to this.joinedRecords.
+     */
+    private void buildAndProbe(Partition leftPartition, Partition rightPartition) {
+        // true if the probe records come from the left partition, false otherwise
+        boolean probeFirst;
+        // We'll build our in memory hash table with these records
+        Iterable<Record> buildRecords;
+        // We'll probe the table with these records
+        Iterable<Record> probeRecords;
+        // The index of the join column for the build records
+        int buildColumnIndex;
+        // The index of the join column for the probe records
+        int probeColumnIndex;
+
+        if (leftPartition.getNumPages() <= this.numBuffers - 2) {
+            buildRecords = leftPartition;
+            buildColumnIndex = getLeftColumnIndex();
+            probeRecords = rightPartition;
+            probeColumnIndex = getRightColumnIndex();
+            probeFirst = false;
+        } else if (rightPartition.getNumPages() <= this.numBuffers - 2) {
+            buildRecords = rightPartition;
+            buildColumnIndex = getRightColumnIndex();
+            probeRecords = leftPartition;
+            probeColumnIndex = getLeftColumnIndex();
+            probeFirst = true;
+        } else {
+            throw new IllegalArgumentException(
+                "Neither the left nor the right records in this partition " +
+                "fit in B-2 pages of memory."
+            );
+        }
+        // TODO(proj3_part1): implement the building and probing stage
+        // You shouldn't refer to any variable starting with "left" or "right"
+        // here, use the "build" and "probe" variables we set up for you.
+        // Check out how SHJOperator implements this function if you feel stuck.
+        HashMap<DataBox, List<Record>> hashTable = new HashMap<>();
+//        Build
+        for(Record buildRecord : buildRecords) {
+            DataBox buildColumnValue = buildRecord.getValue(buildColumnIndex);
+
+            if(!hashTable.containsKey(buildColumnValue)) {
+                hashTable.put(buildColumnValue, new ArrayList<>());
+            }
+
+            hashTable.get(buildColumnValue).add(buildRecord);
+        }
+
+//        Probe
+        for(Record probeRecord : probeRecords) {
+            DataBox probeColumnValue = probeRecord.getValue(probeColumnIndex);
+
+            if(!hashTable.containsKey(probeColumnValue)) continue;
+
+            for(Record buildRecord : hashTable.get(probeColumnValue)) {
+                Record result = probeFirst ? probeRecord.concat(buildRecord) : buildRecord.concat(probeRecord);
+                joinedRecords.add(result);
+            }
+        }
+
+    }
+
+    /**
+     * Runs the grace hash join algorithm. Each pass starts by partitioning
+     * leftRecords and rightRecords. If we can run build and probe on a
+     * partition we should immediately do so, otherwise we should apply the
+     * grace hash join algorithm recursively to break up the partitions further.
+     */
+    private void run(Iterable<Record> leftRecords, Iterable<Record> rightRecords, int pass) {
+        assert pass >= 1;
+        if (pass > 5) throw new IllegalStateException("Reached the max number of passes");
+
+        // Create empty partitions
+        Partition[] leftPartitions = createPartitions(true);
+        Partition[] rightPartitions = createPartitions(false);
+
+        // Partition records into left and right
+        this.partition(leftPartitions, leftRecords, true, pass);
+        this.partition(rightPartitions, rightRecords, false, pass);
+
+        for (Partition leftPartition : leftPartitions) { // 已经分了这么多个区
+            // TODO(proj3_part1): implement the rest of grace hash join
+            // If you meet the conditions to run the build and probe you should
+            // do so immediately. Otherwise you should make a recursive call.
+            for (Partition rightPartition : rightPartitions) {
+                try {
+                    this.buildAndProbe(leftPartition, rightPartition);
+                } catch (Exception IllegalArgumentException) {
+                    // 说明两个分区都很大,接着对left和right分区
+                    run(leftPartition, rightPartition, pass + 1);
+                }
+            }
+        }
+    }
+
+    // Provided Helpers ////////////////////////////////////////////////////////
+
+    /**
+     * Create an appropriate number of partitions relative to the number of
+     * available buffers we have.
+     *
+     * @return an array of partitions
+     */
+    private Partition[] createPartitions(boolean left) {
+        int usableBuffers = this.numBuffers - 1;
+        Partition partitions[] = new Partition[usableBuffers];
+        for (int i = 0; i < usableBuffers; i++) {
+            partitions[i] = createPartition(left);
+        }
+        return partitions;
+    }
+
+    /**
+     * Creates either a regular partition or a smart partition depending on the
+     * value of this.useSmartPartition.
+     * @param left true if this partition will store records from the left
+     *             relation, false otherwise
+     * @return a partition to store records from the specified partition
+     */
+    private Partition createPartition(boolean left) {
+        Schema schema = getRightSource().getSchema();
+        if (left) schema = getLeftSource().getSchema();
+        return new Partition(getTransaction(), schema);
+    }
+
+    // Student Input Methods ///////////////////////////////////////////////////
+
+    /**
+     * Creates a record using val as the value for a single column of type int.
+     * An extra column containing a 500 byte string is appended so that each
+     * page will hold exactly 8 records.
+     *
+     * @param val value the field will take
+     * @return a record
+     */
+    private static Record createRecord(int val) {
+        String s = new String(new char[500]);
+        return new Record(val, s);
+    }
+
+    /**
+     * This method is called in testBreakSHJButPassGHJ.
+     *
+     * Come up with two lists of records for leftRecords and rightRecords such
+     * that SHJ will error when given those relations, but GHJ will successfully
+     * run. createRecord(int val) takes in an integer value and returns a record
+     * with that value in the column being joined on.
+     *
+     * Hints: Both joins will have access to B=6 buffers and each page can fit
+     * exactly 8 records.
+     *
+     * SHJ 5个分区， 有页数超过4页就会break
+     *
+     * @return Pair of leftRecords and rightRecords
+     */
+    public static Pair<List<Record>, List<Record>> getBreakSHJInputs() {
+        ArrayList<Record> leftRecords = new ArrayList<>();
+        ArrayList<Record> rightRecords = new ArrayList<>();
+        // 41条记录就行了
+        // TODO(proj3_part1): populate leftRecords and rightRecords such that
+        // SHJ breaks when trying to join them but not GHJ
+        for(int i = 0; i < 161; i++) {
+            leftRecords.add(createRecord(i));
+            rightRecords.add(createRecord(i));
+        }
+        return new Pair<>(leftRecords, rightRecords);
+    }
+
+    /**
+     * This method is called in testGHJBreak.
+     *
+     * Come up with two lists of records for leftRecords and rightRecords such
+     * that GHJ will error (in our case hit the maximum number of passes).
+     * createRecord(int val) takes in an integer value and returns a record
+     * with that value in the column being joined on.
+     *
+     * Hints: Both joins will have access to B=6 buffers and each page can fit
+     * exactly 8 records.
+     *
+     * 分割超过5步就会崩溃
+     * @return Pair of leftRecords and rightRecords
+     */
+    public static Pair<List<Record>, List<Record>> getBreakGHJInputs() {
+        ArrayList<Record> leftRecords = new ArrayList<>();
+        ArrayList<Record> rightRecords = new ArrayList<>();
+        // TODO(proj3_part1): populate leftRecords and rightRecords such that GHJ breaks
+        for(int i = 0; i < 160; i++) {
+            leftRecords.add(createRecord(1));
+            rightRecords.add(createRecord(1));
+        }
+//        2--3--4---5
+        for(int i = 0; i < 160; i++) {
+            leftRecords.add(createRecord(2));
+            leftRecords.add(createRecord(5));
+            leftRecords.add(createRecord(4));
+            leftRecords.add(createRecord(3));
+        }
+        return new Pair<>(leftRecords, rightRecords);
+    }
+}
+
